91e5e1e78d7a616aab4d90c51d6008637b0bfe46
John840001 <83065836+John840001@users.noreply.github.com>
2024-03-11 00:03:51 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdPLc245dat8df0cNvKyg8_3AheAjU9qk",
-  authDomain: "skill-verify.firebaseapp.com",
-  projectId: "skill-verify",
-  storageBucket: "skill-verify.appspot.com",
-  messagingSenderId: "329519788601",
-  appId: "1:329519788601:web:ca75d40f4d6287a13413e8",
-  measurementId: "G-N1ZBCYV3YZ",
+  apiKey: "test",
+  authDomain: "test",
+  projectId: "test",
+  storageBucket: "test",
+  messagingSenderId: "test",
+  appId: "test",
+  measurementId: "test",
 };
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
